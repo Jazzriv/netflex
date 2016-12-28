3658cc274e5bd7363844b583d79043d02655da2c
OliviaB14 <olivia.boumokonia9@etu.univ-lorraine.fr>
2016-12-28 23:32:03 +0100

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,78 @@
+<?php
+	try{
+		//open connection
+		$bdd = new PDO(
+			"mysql:host=localhost;dbname=netflex",
+			"root",
+			""
+		);
+
+		$bdd->exec("SET NAMES 'UTF8'");
+	} 
+	catch(PDOException $e){
+		echo $e->getMessage();
+	}					
+?>
+
+<html>
+	<head>
+		<meta charset="utf-8"/>
+
+		<!-- STYLESHEETS -->
+		<link rel="stylesheet" type="text/css" href="css/index.css"/>
+
+
+		<title>Netflex - Projet informatique</title>
+	</head>
+
+	<body>
+		<header>
+			<img src="img/logo.png" alt="Logo Netlfex" id="logo" />
+		</header>
+
+		<div id="overlay"></div>
+		<div id="main-container">
+
+	
+			<div id="content">
+				<!-- subscription div / popup content-->
+				<div class="panel" id="popup">
+					<h2>S'inscrire</h2>
+					<img class="close" src="img/close.png" alt="Fermer la fenêtre"/>
+					<form id="subform" method="POST" action="test.php">
+						<label for="identifier2">Nom d'utilisateur :</label>
+						<input type="text" name="identifier2" id="identifier2">
+						<label for="password2">Mot de passe :</label>
+						<input type="password" name="password2" id="password2" placeholder="Mot de passe">
+						<input type="password" name="password3" placeholder="Confirmer le mot de passe">
+						<label for="email">Adresse email :</label>
+						<input type="email" name="email" id="email" placeholder="Adresse mail">
+						<input type="submit" value="Inscription" name="subbtn">
+					</form>
+				</div>
+			</div>
+
+
+				<!-- connection div -->
+				<div class="panel" id="connection">
+					<h2>Se connecter</h2>
+					<form id="conform" method="POST" action="test.php">
+						<input type="text" name="identifier1" placeholder="Nom d'utilisateur">
+						<input type="password" name="password1" placeholder="Mot de passe">
+						<input type="submit" value="Connexion" name="conbtn">
+					</form>
+					<div id="popupbtn"><p>Pas encore inscrit ?</p></div>
+				</div>
+			</div>
+
+		</div>
+
+		<!-- SCRIPTS -->
+		<!-- popup script --><script type="text/javascript" src="js/popup.js"></script>
+		<!-- jquery script --><script type="text/javascript" src="js/jquery-3.1.1.js"></script>
+		<?php
+			//close connection
+			$bdd = null;
+		?>
+	</body>
+</html>
